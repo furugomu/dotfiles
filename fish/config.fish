e0f5eb7183a118538ed3d8351142301ebb8f48dd
furugomu <furugomu@gmail.com>
2022-08-10 09:22:03 +0900

    docker rootless

--- fish/config.fish
+++ fish/config.fish
@@ -21,10 +21,15 @@ if test -d $HOME/.rbenv
 end
 
 # volta
-set VOLTA_HOME "$HOME/.volta"
+set -l VOLTA_HOME "$HOME/.volta"
 if test -d $VOLTA_HOME
     set -x PATH $VOLTA_HOME/bin $PATH
 end
 
 # docker rootless
-set -x DOCKER_HOST unix:///{$XDG_RUNTIME_DIR}/docker.sock
+set -l rootless_socket $XDG_RUNTIME_DIR/docker.sock
+if test -S $rootless_socket
+    set -x DOCKER_HOST unix://$rootless_socket
+else
+    set -x DOCKER_HOST unix:///var/run/docker.sock
+end
